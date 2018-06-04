1355b1b93025a6ed2f58b569c1215c88f23b71d7
DESKTOP-DAPVUPS\BudDev - 016 <kh.aneeq@gmail.com>
2018-06-04 12:24:14 -0700

    CPN-2 Laravel_Project_Set_Up

---
+++ app/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CannaPlan;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'first_name','last_name', 'email', 'password','status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
