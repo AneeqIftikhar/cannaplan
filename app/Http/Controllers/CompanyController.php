ba33c3ff7d7f1ff96c7871688ba2414c34f163e9
DESKTOP-DAPVUPS\BudDev - 016 <kh.aneeq@gmail.com>
2018-06-09 11:30:13 -0700

    CPN-A-6 Get All Companies API Added | Add Company API Fixed

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,9 +3,11 @@
 namespace CannaPlan\Http\Controllers;
 
 use CannaPlan\Models\Company;
+use CannaPlan\Models\Pitch;
 use Illuminate\Http\Request;
 use CannaPlan\Http\Requests\CompanyRequest;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 class CompanyController extends Controller
 {
     /**
@@ -15,7 +17,13 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        //
+        $user = Auth::user();
+        if($user) {
+            return response()->success($user->companies,"Companies Fetched Successfull");
+        }
+        else{
+            return response()->fail("User Not Authenticated");
+        }
     }
 
     /**
@@ -26,18 +34,39 @@ class CompanyController extends Controller
      */
     public function store(CompanyRequest $request)
     {
-        $input = $request->all();
-        $company = Company::create($input);
-        if($company)
+        try
         {
-            //creating pitch with pitch company name as comapnys orignal company name
-            //creating plan with dummy chapters,sections and topics/charts/tables
+            DB::beginTransaction();
+            $input = $request->all();
+            $company = Company::create($input);
+            if($company)
+            {
+                //creating pitch with pitch company name as comapnys orignal company name
+                $company->pitches()->create(['company_name'=>$request->input('title')]);
+                //creating plan with dummy chapters,sections and topics/charts/tables
+
+                DB::commit();
+                return response()->success($company,'Company Created Successfully');
+            }
+            else
+            {
+                DB::rollback();
+                return response()->fail('Company Could Not be Created');
+            }
         }
-        else
-        {
-            return response()->fail('Company Could Not be Created');
+        catch (\PDOException $ex) {
+            DB::rollback();
+            return response()->fail($ex->getMessage());
         }
-        return response()->success($company,'Company Created Successfully');
+        catch (\Exception $ex) {
+            DB::rollback();
+            return response()->fail($ex->getMessage());
+
+        }
+
+
+
+
     }
 
     /**
@@ -49,7 +78,13 @@ class CompanyController extends Controller
     public function show($id)
     {
         $company = Company::find($id);
-        return response()->success($company,'Company Fetched Successfully');
+        if($company){
+            return response()->success($company,'Company Fetched Successfully');
+        }
+        else{
+            return response()->fail("Company Not Found");
+        }
+
     }
 
 
@@ -63,8 +98,14 @@ class CompanyController extends Controller
      */
     public function update(CompanyRequest $request, $id)
     {
-        Company::where('id', $id)->update($request->all());
-        return response()->success($request->all(),'Company Updated Successfully');
+        $company=Company::where('id', $id)->update($request->all());
+        if($company) {
+            return response()->success($request->all(),'Company Updated Successfully');
+        }
+        else {
+            return response()->fail("Company Update Failed");
+        }
+
 
     }
 
@@ -76,7 +117,13 @@ class CompanyController extends Controller
      */
     public function destroy($id)
     {
-        Company::destroy($id);
-        return response()->success([],'Company Deleted Successfully');
+        $company=Company::destroy($id);
+        if($company){
+            return response()->success([],'Company Deleted Successfully');
+        }
+        else {
+            return response()->fail("Company Deletion Failed");
+        }
+
     }
 }
