3ae301527c8792338a263725d7394a1d105534ff
Huzaifa <huzaifa@dev-rec.com>
2018-07-19 21:06:52 +0500

    CPN-H-30 Taxes API show 50% complete

--- routes/api.php
+++ routes/api.php
@@ -127,7 +127,6 @@ Route::group(['middleware' => ['cors']], function () {
         Route::get('get_financing_by_forecast/{id}' , 'FinancingController@getFinancingByForecast');
 
         /*Tax Routes*/
-        Route::resource('tax' , 'TaxController');
         Route::post('update_tax/{id}','TaxController@updateTax');
         Route::get('get_tax_by_forecast/{id}' , 'TaxController@getTaxByForecast');
     });
